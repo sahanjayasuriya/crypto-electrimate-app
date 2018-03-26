6f07b018cd51ab19affe2b8d11830db27763ca9f
Deshani <deshani.2015582@iit.ac.lk>
2018-03-26 23:16:27 +0530

    Implemented function to get module list and display on modules page.

--- src/pages/modules/modules.ts
+++ src/pages/modules/modules.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
 import {ModuleInfoPage} from "../module-info/module-info";
+import {AngularFireAuth} from "angularfire2/auth";
+import {AngularFireDatabase} from "angularfire2/database";
 
 /**
  * Generated class for the ModulesPage page.
@@ -16,15 +18,49 @@ import {ModuleInfoPage} from "../module-info/module-info";
 })
 export class ModulesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    moduleId: string;
+    userId: string;
+    modules:Array<string>;
+    moduleList:Array<{id:string,name:string}> = [];
+
+    constructor(private angularFireDatabase: AngularFireDatabase,
+              private angularFireAuth: AngularFireAuth,
+              public navCtrl: NavController,
+              public navParams: NavParams,
+              public alertController: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ModulesPage');
+      this.userId = this.angularFireAuth.auth.currentUser.uid;
+      console.log(this.userId);
+      this.angularFireDatabase.database.ref('/users/' + this.userId +'/modules/').once('value')
+          .then((data) => {
+              this.modules = data.val();
+              for(let module of this.modules){
+                this.moduleId = module;
+                console.log(this.moduleId);
+                this.angularFireDatabase.database.ref('/modules/'+this.moduleId).once('value')
+                    .then((data) =>{
+                            console.log(data.val().batchNumber);
+                            console.log(data.val().moduleName);
+                            this.moduleList.push({id:this.moduleId,name:data.val().moduleName});
+                        })
+              }
+          }, (error) => {
+              let alert = this.alertController.create({
+                  title: 'No Modules to Show',
+                  subTitle: 'Sorry, we couldn\'t identify the sensor. Please try again or contact support',
+                  buttons: ['OK']
+              });
+              alert.present();
+          });
   }
 
-  viewModule() {
-    this.navCtrl.push(ModuleInfoPage);
+  viewModule(moduleId : string) {
+    // this.navCtrl.push(ModuleInfoPage);
+      console.log(moduleId);
   }
 
 }
