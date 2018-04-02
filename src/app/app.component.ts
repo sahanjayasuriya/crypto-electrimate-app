ad6ba490ea6f1c0b46df58afbb70ece9bd8d9fb8
Sahan Jayasuriya <sahanjayasuriya@hotmail.com>
2018-04-02 09:55:25 +0530

    Added function list excel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,38 +27,41 @@ export class MyApp {
 
     pages: Array<{ title: string, component: any, icon: string }>;
 
-    constructor(platform: Platform,
+    constructor(private platform: Platform,
                 private statusBar: StatusBar,
                 private splashScreen: SplashScreen,
                 private events: Events,
                 private angularFireAuth: AngularFireAuth,
                 private angularFireDatabase: AngularFireDatabase,
-                public alertCtrl: AlertController) {
+                private alertCtrl: AlertController) {
         platform.ready().then(() => {
-            // Okay, so the platform is ready and our plugins are available.
-            // Here you can do any higher level native things you might need.
             this.statusBar.styleDefault();
             this.splashScreen.hide();
         });
 
+        //Setting the current user in sidemenu when the user:logged event fires from the welcome.ts file
         events.subscribe('user:logged', (user, time) => {
-            this.currentUser = user;
+            this.currentUser = user; //Setting the current user object
+
+            //Setting the values of the user object from the user object got from the fired event
             this.user.email = user.email;
             this.user.displayName = user.displayName;
             this.user.photoURL = user.photoURL;
             this.user.phoneNumber = user.phoneNumber;
 
+            //Getting the user type of the logged in user from firebase
+            //Path /users/userId/userType
             this.angularFireDatabase.database.ref('users/' + user.uid + '/userType').once('value')
                 .then((userType) => {
                     console.log(userType.val());
-                    if('HOUSE-OWNER' == userType.val()){
+                    if ('HOUSE-OWNER' == userType.val()) { //Setting the sidemenu items for House Owners
                         this.pages = [
                             {title: 'Home', component: HomePage, icon: 'home'},
                             {title: 'Users', component: UsersPage, icon: 'contacts'},
                             {title: 'Modules', component: ModulesPage, icon: 'outlet'},
                             {title: 'Settings', component: SettingsPage, icon: 'settings'}
                         ];
-                    } else if('ELECTRICITY-USER' == userType.val()){
+                    } else if ('ELECTRICITY-USER' == userType.val()) { //Setting the sidemenu items for Electricity Users
                         this.pages = [
                             {title: 'Home', component: HomePage, icon: 'home'},
                             {title: 'Settings', component: SettingsPage, icon: 'settings'}
@@ -67,11 +70,13 @@ export class MyApp {
                         this.pages = [];
                     }
                 }).catch((err) => {
-
+                console.log(err); //Logging if any error occurred
             });
         });
 
+        //Updating the current user in sidemenu when the user:updated event fires from the user-profile.ts file
         events.subscribe('user:updated', (user, time) => {
+            //Updating the values
             this.currentUser = this.angularFireAuth.auth.currentUser;
             this.user.email = this.currentUser.email;
             this.user.displayName = this.currentUser.displayName;
@@ -79,47 +84,41 @@ export class MyApp {
             this.user.phoneNumber = this.currentUser.phoneNumber;
         });
 
-        this.pages = [
-            {title: 'Home', component: HomePage, icon: 'home'},
-            {title: 'Users', component: UsersPage, icon: 'contacts'},
-            {title: 'Modules', component: ModulesPage, icon: 'outlet'},
-            {title: 'Settings', component: SettingsPage, icon: 'settings'}
-        ];
-
-        this.activePage = this.pages[0];
+        this.activePage = this.pages[0]; //Setting the active page as the first object in the pages array
     }
 
+    //Function to open the specific page when a menu item is touched
     openPage(page) {
         this.nav.setRoot(page.component);
         this.activePage = page;
     }
 
+    //Function to check if the provided page is the active page
     checkActive(page) {
         return page == this.activePage;
     }
 
+    //Logout function
     logOut() {
-        let confirm = this.alertCtrl.create({
+        let confirm = this.alertCtrl.create({   //Displaying a confirm alert
             title: 'Log Out',
             message: 'Are you sure you want to log out?',
             buttons: [
                 {
                     text: 'No',
                     handler: () => {
-                        console.log('No clicked');
-                    }
+                    }   //Code should be written here if the app needs to do anything when No is touched
                 },
                 {
                     text: 'Yes',
                     handler: () => {
-                        console.log('Yes clicked');
-                        this.angularFireAuth.auth.signOut();
-                        this.nav.setRoot(WelcomePage);
+                        this.angularFireAuth.auth.signOut(); //Signing out from firebase
+                        this.nav.setRoot(WelcomePage);      //Setting the root to the login (welcome) page
                     }
                 }
             ],
             cssClass: 'electrimateAlertCss'
         });
-        confirm.present();
+        confirm.present(); //Presenting the alert
     }
 }
